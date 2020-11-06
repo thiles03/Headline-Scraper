c9a1b010698913ebd66c1ce16b776e2797ffcdeb
thiles03 <thiles03@gmail.com>
2020-11-06 20:04:34 +0000

    Initial commit
    Headline scraper

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HeadlineScraper
  News headline web scraper.
+
+Web scraper that collates the headlines from popular world news sites.
+
+Creates a .csv file with today's date.
---
+++ scraper.py
@@ -0,0 +1,138 @@
+import datetime
+import urllib.request as request
+from bs4 import BeautifulSoup as soup
+
+# setup the file
+date = datetime.datetime.now().strftime("%A, %d, %b, %Y")
+filename = "DailyHeadlines - " + date + ".csv"
+f = open(filename, "w")
+
+headers = "Website, Headline\n"
+f.write(headers)
+
+# array of websites to scrape
+newspages = ["https://www.bbc.com/news/world", # BBC
+"https://edition.cnn.com/world", # CNN
+"https://www.nytimes.com/section/world", # NYT
+"https://www.theguardian.com/world", # Guardian
+"https://www.rt.com/news/"] # RT
+
+index = 0
+
+for newspage in newspages:
+
+    # open a connection and grab the page
+    uClient = request.urlopen(newspages[index])
+    htmlPage = uClient.read()
+    uClient.close()
+
+    # parse the html
+    soupPage = soup(htmlPage, "html.parser")
+
+    # check for BBC
+    if index == 0:
+
+        # grab each headline
+        containers = soupPage.findAll("a", {"class":"gs-c-promo-heading"})
+
+        # write the headline to .csv file
+        for container in containers:
+
+            website = "BBC World News"
+
+            try:
+                headline = container.h3.text
+            except:
+                headline = ""
+
+            if headline == "":
+                try:
+                    headline = container.text.strip()
+                except:
+                    headline = ""
+
+            f.write(website + "," + headline.replace(",", "|").replace("Video", "(Video) ") + "\n")
+
+    #check for CNN
+    if index == 1:
+
+        # grab each headline
+        containers = soupPage.findAll("h3", {"class":"cd__headline"}) # not finding all
+
+        # write the headline to .csv file
+        for container in containers:
+
+            website = "CNN World News"
+            
+            try:
+                headline = container.a.span.text
+            except:
+                headline = ""
+
+            f.write(website + "," + headline.replace(",", "|") + "\n")
+
+    # check for NYT
+    if index == 2:
+
+        # grab each headline
+        containers = soupPage.findAll("div", {"class":["css-10wtrbd","css-1l4spti"]})
+
+        # write the headline to .csv file
+        for container in containers:
+
+            website = "The New York Times"
+            
+            try:
+                headline = container.h2.a.text
+            except:
+                headline = ""
+
+            if headline == "":
+                try:
+                    headline = container.a.h2.text
+                except:
+                    headline = ""
+
+            f.write(website + "," + headline.replace(",", "|") + "\n")
+
+    # check for Guardian
+    if index == 3:
+
+        # grab each headline
+        containers = soupPage.findAll("h3", {"class":"fc-item__title"})
+
+        # write the headline to .csv file
+        for container in containers:
+
+            website = "The Guardian"
+            
+            try:
+                headline = container.a.text.strip()
+            except:
+                headline = ""
+
+            f.write(website + "," + headline.replace(",", "|").replace("\n", "") + "\n")
+
+    # check for RT
+    if index == 4:
+
+        # grab each headline
+        containers = soupPage.findAll("strong", {"class":"card__header"})
+
+        # write the headline to .csv file
+        for container in containers:
+
+            website = "RT"
+            
+            try:
+                headline = container.a.text.strip()
+            except:
+                headline = ""
+
+            f.write(website + "," + headline.replace(",", "|") + "\n")
+
+            
+    
+    index +=1
+
+f.close()
