d754c5f6d0c52831f3a01723203b3a62a5b288b2
thiles03 <thiles03@gmail.com>
2020-11-16 16:06:58 +0000

    Refactor

--- scraper.py
+++ scraper.py
@@ -1,212 +1,266 @@
 import os
+import io
 import datetime
+from emoji import demojize
+from unicodedata import normalize
 from pathlib import Path
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 
-# setup the file
-date = datetime.datetime.now().strftime("%d, %A, %b, %Y")
-folder = datetime.datetime.now().strftime("%Y %m%b")
-filepath = Path("Outputs/" + folder)
-
-if not os.path.exists(filepath):
-    os.makedirs(filepath)
-
-filename = Path("Outputs/" + folder + "/DailyHeadlines - " + date + ".csv")
-f = open(filename, "w")
-
-headers = "Website, Headline, Link\n"
-f.write(headers)
 
 # array of websites to scrape
-newspages = ["https://www.bbc.com/news/world", # BBC
-"https://www.reuters.com/", # Reutuers
-"https://www.nytimes.com/section/world", # NYT
-"https://www.theguardian.com/world", # Guardian
-"https://www.rt.com/news/", # RT
-"https://www.wsj.com/news/world"] # WSJ
+NEWSPAGES = ["https://www.bbc.com/news/world",  # BBC
+             "https://www.reuters.com/",  # Reutuers
+             "https://www.nytimes.com/section/world",  # NYT
+             "https://www.theguardian.com/world",  # Guardian
+             "https://www.rt.com/news/",  # RT
+             "https://www.wsj.com/news/world"]  # WSJ
 
-index = 0
 
-for newspage in newspages:
+def setupFile():
 
-    # open a connection and grab the page
+    date = datetime.datetime.now().strftime("%d, %A, %b, %Y")
+    folder = datetime.datetime.now().strftime("%Y %m%b")
+    filepath = Path("Outputs/" + folder)
 
-    req = Request(newspages[index], headers={'User-Agent': 'Mozilla/5.0'})
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
 
-    uClient = urlopen(req)
-    htmlPage = uClient.read()
-    uClient.close()
+    filename = Path("Outputs/" + folder + "/DailyHeadlines - " + date + ".csv")
+    f = io.open(filename, "w", encoding="utf8")
 
-    # parse the html
-    soupPage = soup(htmlPage, "html.parser")
+    headers = "Website, Headline, Link\n"
+    f.write(headers)
 
-    # check for BBC
-    if index == 0:
+    return f
 
-        # grab each headline container
-        containers = soupPage.findAll("a", {"class":"gs-c-promo-heading"})
 
-        # write the headline and link to .csv
-        for container in containers:
+def normalise(string):
 
-            website = "BBC World News"
+    normalisedString = demojize(string)
+    return normalisedString
 
-            try:
-                headline = container.text.strip()
-            except:
-                headline = "null"
 
-            try:
-                link = "=HYPERLINK(\"https://www.bbc.com" + container.get("href") + "\")"
-            except:
-                link = "null"
+def format(website, headline, link):
 
-            f.write(website + "," + headline.replace(",", "|").replace("Video", "(Video) ") + "," + link + "\n")
+    fString = website + "," + headline.replace(",", "|").replace("Video", "(Video) ").replace(
+        "\n", "") + "," + link + "\n"
+    return fString
 
-    #check for Reutuers
-    if index == 1:
 
-        # grab each headline container
-        containers = soupPage.findAll("h3", {"class":"article-heading"})
-        containers.extend(soupPage.findAll("div", {"class":"story-content"}))
-        containers.extend(soupPage.findAll("article", {"class":"story-content"}))
+def main():
 
-        # write the headline and link to .csv
-        for container in containers:
+    f = setupFile()
 
-            website = "Reutuers"
-            
-            try:
-                headline = container.a.h3.text
-            except:
+    index = 0
+
+    for page in NEWSPAGES:
+
+        # open a connection and grab the page
+        req = Request(NEWSPAGES[index], headers={'User-Agent': 'Mozilla/5.0'})
+
+        uClient = urlopen(req)
+        htmlPage = uClient.read()
+        uClient.close()
+
+        # parse the html
+        soupPage = soup(htmlPage, "html.parser")
+
+        # check for BBC
+        if index == 0:
+
+            # grab each headline container
+            containers = soupPage.findAll("a", {"class": "gs-c-promo-heading"})
+
+            # write the headline and link to .csv
+            for container in containers:
+
+                website = "BBC World News"
+
                 try:
-                    headline = container.h3.a.text
+                    headline = container.text.strip()
+                except:
+                    headline = "null"
+
+                try:
+                    link = "=HYPERLINK(\"https://www.bbc.com" + \
+                        container.get("href") + "\")"
+                except:
+                    link = "null"
+
+                headline = normalise(headline)
+                fileline = format(website, headline, link)
+                f.write(fileline)
+
+        # check for Reutuers
+        if index == 1:
+
+            # grab each headline container
+            containers = soupPage.findAll("h3", {"class": "article-heading"})
+            containers.extend(soupPage.findAll(
+                "div", {"class": "story-content"}))
+            containers.extend(soupPage.findAll(
+                "article", {"class": "story-content"}))
+
+            # write the headline and link to .csv
+            for container in containers:
+
+                website = "Reutuers"
+
+                try:
+                    headline = container.a.h3.text
                 except:
                     try:
-                        headline = container.a.text
+                        headline = container.h3.a.text
+                    except:
+                        try:
+                            headline = container.a.text
+                        except:
+                            headline = "null"
+
+                try:
+                    link = "=HYPERLINK(\"https://www.reuters.com" + \
+                        container.a.get("href") + "\")"
+                except:
+                    link = "null"
+
+                headline = normalise(headline)
+                fileline = format(website, headline, link)
+                f.write(fileline)
+
+        # check for NYT
+        if index == 2:
+
+            # grab each headline container
+            containers = soupPage.findAll(
+                "div", {"class": ["css-10wtrbd", "css-1l4spti"]})
+
+            # write the headline and link to .csv
+            for container in containers:
+
+                website = "The New York Times"
+
+                try:
+                    headline = container.h2.a.text
+                except:
+                    try:
+                        headline = container.a.h2.text
                     except:
                         headline = "null"
 
-            try:
-                link = "=HYPERLINK(\"https://www.reuters.com" + container.a.get("href") + "\")"
-            except:
-                link = "null"
-
-            f.write(website + "," + headline.replace(",", "|").replace("\n", "") + "," + link + "\n")
-
-    # check for NYT
-    if index == 2:
-
-        # grab each headline container
-        containers = soupPage.findAll("div", {"class":["css-10wtrbd","css-1l4spti"]})
-
-        # write the headline and link to .csv
-        for container in containers:
-
-            website = "The New York Times"
-            
-            try:
-                headline = container.h2.a.text
-            except:
                 try:
-                    headline = container.a.h2.text
-                except:
-                    passheadline = "null"
-
-            try:
-                link = "=HYPERLINK(\"https://www.nytimes.com" + container.h2.a.get("href") + "\")"
-            except:
-                try:
-                    link = "=HYPERLINK(\"https://www.nytimes.com" + container.a.get("href") + "\")"
-                except:
-                    link = "null"            
-
-            f.write(website + "," + headline.replace(",", "|") + "," + link + "\n")
-
-    # check for Guardian
-    if index == 3:
-
-        # grab each headline container
-        containers = soupPage.findAll("h3", {"class":"fc-item__title"})
-
-        # write the headline and link to .csv
-        for container in containers:
-
-            website = "The Guardian"
-            
-            try:
-                headline = container.a.text.strip()
-            except:
-                headline = "null"
-
-            try:
-                link = "=HYPERLINK(\"" + container.a.get("href") + "\")"
-            except:
-                link = "null"
-
-
-            f.write(website + "," + headline.replace(",", "|").replace("\n", "") + "," + link + "\n")
-
-    # check for RT
-    if index == 4:
-
-        # grab each headline container
-        containers = soupPage.findAll("strong", {"class":"card__header"})
-
-        # write the headline and link to .csv
-        for container in containers:
-
-            website = "RT"
-            
-            try:
-                headline = container.a.text.strip()
-            except:
-                headline = "null"
-
-            try:
-                link = "=HYPERLINK(\"https://www.rt.com" + container.a.get("href") + "\")"
-            except:
-                link = "null"
-
-            f.write(website + "," + headline.replace(",", "|") + "," + link + "\n")
-
-    # check for WSJ
-    if index == 5:
-
-        # grab each headline container
-        containers = soupPage.findAll("div", {"class":"WSJTheme--headline--7VCzo7Ay"})
-        containers.extend(soupPage.findAll("a", {"class":"style--link--1n1ZR5I9"}))
-
-        # write the headline and link to .csv
-        for container in containers:
-
-            website = "Wall Street Journal"
-            
-            try:
-                headline = container.h2.a.text
-            except:
-                try:
-                   headline = container.h3.a.text
+                    link = "=HYPERLINK(\"https://www.nytimes.com" + \
+                        container.h2.a.get("href") + "\")"
                 except:
                     try:
-                        headline = container.h3.span.text.strip()
-                    except:
-                        headline = "null"
-
-            try:
-                link = "=HYPERLINK(\"" + container.h2.a.get("href") + "\")"
-            except:
-                try:
-                    link = "=HYPERLINK(\"" + container.h3.a.get("href") + "\")"
-                except:
-                    try:
-                        link = "=HYPERLINK(\"" + container.get("href") + "\")"
+                        link = "=HYPERLINK(\"https://www.nytimes.com" + \
+                            container.a.get("href") + "\")"
                     except:
                         link = "null"
 
-            f.write(website + "," + headline.replace(",", "|") + "," + link + "\n")
+                headline = normalise(headline)
+                fileline = format(website, headline, link)
+                f.write(fileline)
 
-    index +=1
+        # check for Guardian
+        if index == 3:
 
-f.close()
+            # grab each headline container
+            containers = soupPage.findAll("h3", {"class": "fc-item__title"})
+
+            # write the headline and link to .csv
+            for container in containers:
+
+                website = "The Guardian"
+
+                try:
+                    headline = container.a.text.strip()
+                except:
+                    headline = "null"
+
+                try:
+                    link = "=HYPERLINK(\"" + container.a.get("href") + "\")"
+                except:
+                    link = "null"
+
+                headline = normalise(headline)
+
+                f.write(website + "," + headline.replace(",",
+                                                         "|").replace("\n", "") + "," + link + "\n")
+
+        # check for RT
+        if index == 4:
+
+            # grab each headline container
+            containers = soupPage.findAll("strong", {"class": "card__header"})
+
+            # write the headline and link to .csv
+            for container in containers:
+
+                website = "RT"
+
+                try:
+                    headline = container.a.text.strip()
+                except:
+                    headline = "null"
+
+                try:
+                    link = "=HYPERLINK(\"https://www.rt.com" + \
+                        container.a.get("href") + "\")"
+                except:
+                    link = "null"
+
+                headline = normalise(headline)
+
+                f.write(website + "," + headline.replace(",", "|") +
+                        "," + link + "\n")
+
+        # check for WSJ
+        if index == 5:
+
+            # grab each headline container
+            containers = soupPage.findAll(
+                "div", {"class": "WSJTheme--headline--7VCzo7Ay"})
+            containers.extend(soupPage.findAll(
+                "a", {"class": "style--link--1n1ZR5I9"}))
+
+            # write the headline and link to .csv
+            for container in containers:
+
+                website = "Wall Street Journal"
+
+                try:
+                    headline = container.h2.a.text
+                except:
+                    try:
+                        headline = container.h3.a.text
+                    except:
+                        try:
+                            headline = container.h3.span.text.strip()
+                        except:
+                            headline = "null"
+
+                try:
+                    link = "=HYPERLINK(\"" + container.h2.a.get("href") + "\")"
+                except:
+                    try:
+                        link = "=HYPERLINK(\"" + \
+                            container.h3.a.get("href") + "\")"
+                    except:
+                        try:
+                            link = "=HYPERLINK(\"" + \
+                                container.get("href") + "\")"
+                        except:
+                            link = "null"
+
+                headline = normalise(headline)
+
+                f.write(website + "," + headline.replace(",", "|") +
+                        "," + link + "\n")
+
+        index += 1
+
+    f.close()
+
+
+main()
